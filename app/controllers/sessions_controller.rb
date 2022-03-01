07fab5db3103b786b64da00a1af46161cf484b10
Vivian Yim <keroviv86@gmail.com>
2022-03-01 12:32:59 -0600

    commit tree 027fe40ce06305ea6d90f23278770dbf77106603

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,15 @@ class SessionsController < ApplicationController
         user = User.find_by(username: params[:username])
         session[:user_id] = user.id
         render json: user
+        
       end
     
       def destroy
         session.delete :user_id
         head :no_content
       end
+
+      
     
 
 end
